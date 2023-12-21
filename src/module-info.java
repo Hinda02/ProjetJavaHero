f07010d7f0da028a69cd9dad31994e90587b0ffc
MbolatianaR <mbolatiana.rabia@gmail.com>
2023-12-22 00:24:35 +0100

    creating MagicalWeaponTest (JUnit)

--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,6 @@
  */
 module MythologyHeroes {
 	requires java.desktop;
+	requires org.junit.jupiter.api;
+	requires junit;
 }
---
+++ src/univers/tools/MagicalWeaponTest.java
@@ -0,0 +1,22 @@
+package univers.tools;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MagicalWeaponTest {
+
+	@Test
+    public void toString_magicalWeapon_returnsDescription() {
+        MagicalWeapon magicalWeapon = new MagicalWeapon("Susanoo", WeaponType.Sword, Attribute.Wind);
+        assertEquals("Susanoo", magicalWeapon.getName());
+        assertEquals(10, magicalWeapon.getPower());
+        magicalWeapon.setPower(20);
+        assertEquals(20, magicalWeapon.getPower());
+        assertEquals(Attribute.Wind, magicalWeapon.getAttribute());
+        assertEquals("Susanoo est une arme de type Sword et dont la puissance est de 20.\nC'est une arme magique dont l'attribut est Wind", magicalWeapon.toString());
+    }
+	
+	
+
+}
