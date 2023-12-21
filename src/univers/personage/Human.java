589bf1ab2d02a5e1de089ef04cbaba5723bb1fe1
MbolatianaR <mbolatiana.rabia@gmail.com>
2023-12-21 16:53:10 +0100

    toString equals Human

--- src/univers/personage/Human.java
+++ src/univers/personage/Human.java
@@ -1,7 +1,6 @@
 package univers.personage;
 
 import univers.Familiar;
-import univers.tools.Item;
 import univers.tools.NonMagicalWeapon;
 
 //class inherits from Personage
@@ -20,10 +19,18 @@ public class Human extends Hero{
 		this.setFamiliar(familiar);
 	}
 	
+	/**
+	 * returns the human's familiar 
+	 * @return Familiar 
+	 */
 	public Familiar getFamiliar() {
 		return familiar;
 	}
-
+	
+	/**
+	 * modifies the human's familiar
+	 * @param familiar
+	 */
 	public void setFamiliar(Familiar familiar) {
 		this.familiar = familiar;
 	}
@@ -37,15 +44,38 @@ public class Human extends Hero{
 		}
 		
 	}
-
+	/**
+	 * returns the human's info
+	 * @return String
+	 */
+	@Override
+	public String toString() {
+		
+		String result = super.toString()  + "\nVotre familier : " + familiar.toString(); 
+		return result;
+	}
 	
-
-	
-
-
-
-	
-	
-	
+	/**
+	 * compares two objects of type Human
+	 * @return boolean
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == this) {
+			return true;
+		}
+		
+		if (!(obj instanceof Human)) {
+			return false;
+		}
+		
+		Human human = (Human) obj;
+		
+		if(super.equals(human) && (this.familiar.equals(human.familiar))) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 
 }
