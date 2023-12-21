630179b92da20db0d6217304afcc48a932e383e9
MbolatianaR <mbolatiana.rabia@gmail.com>
2023-12-21 17:22:08 +0100

    Minion toString and equals

--- src/univers/personage/Minion.java
+++ src/univers/personage/Minion.java
@@ -4,18 +4,23 @@ import univers.tools.Weapon;
 
 public class Minion extends Villain{
 	
-
+	/**
+	 * Constructor of Minion class
+	 * @param name
+	 * @param weapon
+	 * @param attackSpeed
+	 */
 	public Minion(String name, Weapon weapon, int attackSpeed) {
 		super(name, weapon, attackSpeed);
 	}
 
-
+	/**
+	 * Before a combat, the minion presents himself 
+	 */
 	public void speak() {
-		System.out.println("pour Tiana");
-		
+		System.out.println("Mon nom est "+ this.getName()+" !\nComment oses-tu mettre les pieds sur mon territoire !\nJe vais te punir à l'aide de mon arme, "+this.getWeapon());
 	}
-
-
+	
 	
 
 }
