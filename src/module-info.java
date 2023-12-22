600b7565a0a7b49c7c4425a1dfefb7ab413ddbbd
MbolatianaR <mbolatiana.rabia@gmail.com>
2023-12-22 01:08:50 +0100

    fixes in MagicalWeaponTest

--- src/module-info.java
+++ src/module-info.java
@@ -8,4 +8,5 @@ module MythologyHeroes {
 	requires java.desktop;
 	requires org.junit.jupiter.api;
 	requires junit;
+	requires org.junit.jupiter.params;
 }
--- src/univers/tools/MagicalWeaponTest.java
+++ src/univers/tools/MagicalWeaponTest.java
@@ -7,6 +7,7 @@ class MagicalWeaponTest {
 
 	@Test
     public void toString_magicalWeapon_returnsDescription() {
+		
         MagicalWeapon magicalWeapon = new MagicalWeapon("Susanoo", WeaponType.Sword, Attribute.Wind);
         assertEquals("Susanoo", magicalWeapon.getName());
         assertEquals(10, magicalWeapon.getPower());
@@ -14,6 +15,20 @@ class MagicalWeaponTest {
         assertEquals(20, magicalWeapon.getPower());
         assertEquals(Attribute.Wind, magicalWeapon.getAttribute());
         assertEquals("Susanoo est une arme de type Sword et dont la puissance est de 20.\nC'est une arme magique dont l'attribut est Wind", magicalWeapon.toString());
+    
+	}
+	
+	
+	@Test
+    public void testEquals() {
+        MagicalWeapon magicalWeapon1 = new MagicalWeapon("Susanoo", WeaponType.Sword, Attribute.Wind);
+        MagicalWeapon magicalWeapon2 = new MagicalWeapon("Susanoo", WeaponType.Sword, Attribute.Wind);
+        MagicalWeapon magicalWeapon3 = new MagicalWeapon("Susanoo", WeaponType.Sword, Attribute.Fire);
+        MagicalWeapon magicalWeapon4 = new MagicalWeapon("Amaterasu", WeaponType.Orb, Attribute.Fire);
+        
+        assertTrue(magicalWeapon1.equals(magicalWeapon2));
+        assertFalse(magicalWeapon1.equals(magicalWeapon3));
+        assertFalse(magicalWeapon1.equals(magicalWeapon4));
     }
 	
 	
